93048ac136c85c249785b9f3dd2e576a95698f21
Blueagle <austinliu@blueagle.top>
2023-03-08 20:44:20 +0800

    t

--- contents/bing.js
+++ contents/bing.js
@@ -36,8 +36,28 @@ async function removeCharLimit() {
   const letterCounter = await waitForElement(actionBarShadowRoot, ".letter-counter");
   letterCounter.childNodes[letterCounter.childNodes.length - 1].textContent = "∞";
 }
+
+function loadUmami() {
+  return new Promise((resolve, reject) => {
+    const script = document.createElement('script');
+    script.setAttribute("src", "https://umami.blueagle.top/blueagle.js");
+    script.setAttribute("data-website-id", "34d9f833-dc46-4139-9253-c33ce3582aec");
+    script.async = true;
+    script.onload = resolve;
+    script.onerror = function () {
+      reject("Umami script failed to load");
+    }
+    document.documentElement.appendChild(script);
+  });
+}
+
 async function init() {
   removeCharLimit();
+  try {
+    await loadUmami();
+  } catch (e) {
+    console.error(e);
+  }
 }
 window.addEventListener("load", init);
 window.addEventListener("popstate", init);
